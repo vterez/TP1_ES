9bee825b248935e84b3834c8d4125b6297105b5d
lucasaugs <lucasaugusto83.la@gmail.com>
2021-11-28 21:09:20 -0300

    Dados temporários criados para teste.

---
+++ frontend/src/temp-data/atividades.js
@@ -0,0 +1,48 @@
+export const atividades = [
+    {
+        id:1,
+        id_disciplina:1,
+        atividade_avaliativa_nome:'Prova 1',
+        atividade_avaliativa_nota: 10,
+        atividade_avaliativa_data:'05/12/2021',
+        atividade_avaliativa_nota_obtida:10,
+        conteudo:'Atividade avaliativa'
+    },
+    {
+        id:2,
+        id_disciplina:3,
+        atividade_avaliativa_nome:'TP1',
+        atividade_avaliativa_nota: 10,
+        atividade_avaliativa_data:'15/12/2021',
+        atividade_avaliativa_nota_obtida:10,
+        conteudo:'Atividade avaliativa'
+    },
+    {
+        id:3,
+        id_disciplina:2,
+        atividade_avaliativa_nome:'Lista Exercícios 1',
+        atividade_avaliativa_nota: 10,
+        atividade_avaliativa_data:'30/11/2021',
+        atividade_avaliativa_nota_obtida:10,
+        conteudo:'Atividade avaliativa'
+    },
+    {
+        id:4,
+        id_disciplina:1,
+        atividade_avaliativa_nome:'Prova 2',
+        atividade_avaliativa_nota: 10,
+        atividade_avaliativa_data:'10/01/2022',
+        atividade_avaliativa_nota_obtida:10,
+        conteudo:'Atividade avaliativa'
+    },
+    {
+        id:5,
+        id_disciplina:2,
+        atividade_avaliativa_nome:'TP1',
+        atividade_avaliativa_nota: 10,
+        atividade_avaliativa_data:'06/02/2022',
+        atividade_avaliativa_nota_obtida:10,
+        conteudo:'Atividade avaliativa'
+    },
+    
+]
---
+++ frontend/src/temp-data/disciplinas.js
@@ -0,0 +1,26 @@
+export const disciplinas = [
+    {
+        id: 1,
+        professor:'Marco Tulio',
+        disciplina_nome:'Engenharia de Software',
+        horario:'15h',
+        sala:'ICEx - 1000',
+        carga_horaria:'60h'
+    },
+    {
+        id: 2,
+        professor:'Mariza',
+        disciplina_nome:'Compiladores I',
+        horario:'15h',
+        sala:'ICEx - 2000',
+        carga_horaria:'60h'
+    },
+    {
+        id: 3,
+        professor:'Phablo',
+        disciplina_nome:'Pesquisa Operacional',
+        horario:'17h',
+        sala:'ICEx - 3000',
+        carga_horaria:'60h'
+    }
+]
